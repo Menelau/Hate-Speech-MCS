748108bee7b8e53f52beee0343e2bd3cb69afd11
Menelau-Cruz <rafael.menelau-cruz@etsmtl.ca>
2021-07-15 19:28:08 -0400

    updating jupyter notebooks

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,9 @@ import nltk
 from unicodedata import normalize
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
-import re
 
+
+import re
 def pre_processing(tweet_text):
     # Removendo URLS
     new_text = re.sub(r"http\S+", " ", tweet_text)
@@ -11,7 +12,7 @@ def pre_processing(tweet_text):
     new_text = re.sub('RT @[\w_]+: ', ' ', new_text)
     # Removendo tags
     new_text = re.sub(r"@\S+", " ", new_text)
-    # Retirando caracteres especiais
+    # removing special characters
     new_text = normalize('NFKD', new_text).encode('ASCII', 'ignore').decode('ASCII')
     new_text = re.sub('[0-9]', ' ', str(new_text))
     new_text = re.sub('\s+', ' ', new_text)
